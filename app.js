0e643e88a3bcd836a80700db0b9f1e1bfdf3d6b7
m.ahmed <m.ahmed@veraqor.io>
2025-11-17 18:20:15 +0500

    init

---
+++ app.js
@@ -0,0 +1,36 @@
+const title = document.getElementById("title");
+const input = document.getElementById("todoInput");
+const addBtn = document.getElementById("addBtn");
+const todoList = document.getElementById("todoList");
+
+console.log(title);
+
+title.textContent = "DOM Manipulation Demo (JavaScript)";
+
+addBtn.addEventListener("click", function () {
+  const taskText = input.value.trim();
+
+  if (taskText === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  todoList.appendChild(li);
+
+  input.value = "";
+});
+
+todoList.addEventListener("click", function (event) {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("completed");
+  }
+});
+
+todoList.addEventListener("dblclick", function (event) {
+  if (event.target.tagName === "LI") {
+    event.target.remove();
+  }
+});
