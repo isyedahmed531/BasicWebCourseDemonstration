da42500dc4c9acd2422badabb2de2e1b32247fdc
m.ahmed <m.ahmed@veraqor.io>
2025-11-24 14:59:26 +0500

    feat: classes on Js

--- app.js
+++ app.js
@@ -34,3 +34,21 @@ todoList.addEventListener("dblclick", function (event) {
     event.target.remove();
   }
 });
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+  }
+}
+const alice = new Person("Alice", 30);
+alice.greet();
+
+const bob = new Person("Bob", 25);
+bob.greet();
